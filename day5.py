c981a9d1683bf5f79e4a0b0394fd23813f10bb2a
Becca Saines <rebecca.saines@gmail.com>
2016-12-21 10:06:47 -0800

    Day 5 Part 2 Complete -- 10 STARS

--- day5.py
+++ day5.py
@@ -3,17 +3,23 @@ import hashlib
 text = 'ojvtpuvg'
 # text = 'abc'
 i = 0
-decrypt = ''
+decrypt = [0,0,0,0,0,0,0,0]
+seen = set()
 
-while len(decrypt) < 8:
+while len(seen) < 8:
     curr_test = text + str(i)
     curr_hash = hashlib.md5(curr_test)
     curr_hex = curr_hash.hexdigest()
-    if i % 10000000 == 0:
+    if i % 1000000 == 0:
         print i
     i += 1
     if curr_hex[:5] == '00000':
         print "*****\n", i, curr_hex, "\n*****"
-        decrypt += curr_hex[5]
+        if curr_hex[5].isdigit():
+            index = int(curr_hex[5])
+            if index not in seen and index < 8:
+                seen.add(index)
+                decrypt[index] = curr_hex[6]
+                print "*****\n", index, curr_hex[6], decrypt, "\n*****"
 
-print decrypt
+print ''.join(decrypt)
